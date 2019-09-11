88a23fb97b4f2728794abc64513c1767ff5be554
Matthew Bauer <mjbauer95@gmail.com>
2019-09-11 18:52:10 -0400

    Cleanup test suite

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,4 @@
 import Control.Exception
-import Control.Monad
 import Data.List
 import System.Directory
 import System.Environment
@@ -9,17 +8,19 @@ import System.Posix.Temp
 import System.Which
 
 shouldEqual :: Eq a => Show a => a -> a -> IO ()
-shouldEqual a b = when (a /= b) $ do
+shouldEqual a b | a /= b = do
   putStrLn $ show a <> " does not equal " <> show b
   exitFailure
+shouldEqual a b = putStrLn $ show a <> " == " <> show b
+
+makeExecutable :: FilePath -> IO ()
+makeExecutable f = setPermissions f . setOwnerExecutable True =<< getPermissions f
 
 setup :: IO (FilePath, FilePath, FilePath)
 setup = do
   bin <- mkdtemp "bin"
   bin2 <- mkdtemp "bin2"
   bin3 <- mkdtemp "bin3"
-
-  let makeExecutable f = setPermissions f . setOwnerExecutable True =<< getPermissions f
 
   appendFile (bin </> "hello") "hello"
   makeExecutable $ bin </> "hello"
@@ -43,45 +44,44 @@ cleanup (bin, bin2, bin3) = do
   removeDirectoryRecursive bin3
 
 main :: IO ()
-main = do
-  bracket setup cleanup $ \(bin, bin2, bin3) -> do
-    unsetEnv "PATH"
-    unsetEnv "HOST_PATH"
+main = bracket setup cleanup $ \(bin, bin2, bin3) -> do
+  unsetEnv "PATH"
+  unsetEnv "HOST_PATH"
 
-    fp <- which "hello"
+  fp <- which "hello"
 
-    shouldEqual fp Nothing
+  shouldEqual fp Nothing
 
-    setEnv "PATH" $ concat $ intersperse ":" [bin, bin2, bin3]
-    setEnv "HOST_PATH" $ concat $ intersperse ":" [bin, bin2]
+  setEnv "PATH" $ concat $ intersperse ":" [bin, bin2, bin3]
+  setEnv "HOST_PATH" $ concat $ intersperse ":" [bin, bin2]
 
-    fp1 <- which "hello"
-    fp2 <- which "hello2"
-    fp3 <- which "hello3"
+  fp1 <- which "hello"
+  fp2 <- which "hello2"
+  fp3 <- which "hello3"
 
-    shouldEqual fp1 (Just $ bin </> "hello")
-    shouldEqual fp2 (Just $ bin2 </> "hello2")
-    shouldEqual fp3 Nothing
+  shouldEqual fp1 (Just $ bin </> "hello")
+  shouldEqual fp2 (Just $ bin2 </> "hello2")
+  shouldEqual fp3 Nothing
 
-    unsetEnv "PATH"
+  unsetEnv "PATH"
 
-    fp4 <- which "hello"
-    fp5 <- which "hello2"
-    fp6 <- which "hello3"
+  fp4 <- which "hello"
+  fp5 <- which "hello2"
+  fp6 <- which "hello3"
 
-    shouldEqual fp4 (Just $ bin </> "hello")
-    shouldEqual fp5 (Just $ bin2 </> "hello2")
-    shouldEqual fp6 Nothing
+  shouldEqual fp4 (Just $ bin </> "hello")
+  shouldEqual fp5 (Just $ bin2 </> "hello2")
+  shouldEqual fp6 Nothing
 
-    unsetEnv "HOST_PATH"
-    setEnv "PATH" $ concat $ intersperse ":" [bin3, bin2, bin]
+  unsetEnv "HOST_PATH"
+  setEnv "PATH" $ concat $ intersperse ":" [bin3, bin2, bin]
 
-    fp7 <- which "hello"
-    fp8 <- which "hello2"
-    fp9 <- which "hello3"
+  fp7 <- which "hello"
+  fp8 <- which "hello2"
+  fp9 <- which "hello3"
 
-    shouldEqual fp7 (Just $ bin3 </> "hello")
-    shouldEqual fp8 (Just $ bin2 </> "hello2")
-    shouldEqual fp9 (Just $ bin3 </> "hello3")
+  shouldEqual fp7 (Just $ bin3 </> "hello")
+  shouldEqual fp8 (Just $ bin2 </> "hello2")
+  shouldEqual fp9 (Just $ bin3 </> "hello3")
 
-    pure ()
+  pure ()
